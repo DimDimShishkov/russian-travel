59f273323550f1ed39e39565c2885487cd19cd9f
DimDimShishkov <d.shishkov.russia@gmail.com>
2022-04-04 10:07:17 +0300

    0404

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,44 @@
 function text_en() {
     document.getElementsByClassName('header__lang-link_ru')[0].style.textDecoration = 'none';
     document.getElementsByClassName('header__lang-link_en')[0].style.textDecoration = 'underline';
-    var elements = document.getElementsByClassName("text_ru");
 
-    for(var i = 0; i < elements.length; ++i){
-      elements[i].style.display = 'none';
-    }
-    var elements = document.getElementsByClassName("text_en");
+    document.getElementsByClassName('lead__title_ru')[0].style.display = 'none';
+    document.getElementsByClassName('lead__title_en')[0].style.display = 'block';
+    document.getElementsByClassName('lead__subtitle_ru')[0].style.display = 'none';
+    document.getElementsByClassName('lead__subtitle_en')[0].style.display = 'block';
+    document.getElementsByClassName('lead__caption_ru')[0].style.display = 'none';
+    document.getElementsByClassName('lead__caption_en')[0].style.display = 'block';
 
-    for(var i = 0; i < elements.length; ++i){
-      elements[i].style.display = 'block';
-    }
+    document.getElementsByClassName('intro__title_ru')[0].style.display = 'none';
+    document.getElementsByClassName('intro__title_en')[0].style.display = 'block';
+    document.getElementsByClassName('intro__text_ru')[0].style.display = 'none';
+    document.getElementsByClassName('intro__text_en')[0].style.display = 'block';
+    document.getElementsByClassName('intro__list_ru')[0].style.display = 'none';
+    document.getElementsByClassName('intro__list_en')[0].style.display = 'block';
+
+    document.getElementsByClassName('place__paragraph_ru')[0].style.display = 'none';
+    document.getElementsByClassName('place__paragraph_en')[0].style.display = 'block';
 }
 
 function text_rus() {
     document.getElementsByClassName('header__lang-link_ru')[0].style.textDecoration = 'underline';
     document.getElementsByClassName('header__lang-link_en')[0].style.textDecoration = 'none';
-    var elements = document.getElementsByClassName("text_en");
-    for(var i = 0; i < elements.length; ++i){
-      elements[i].style.display = 'none';
-    }
-    var elements = document.getElementsByClassName("text_ru");
-    for(var i = 0; i < elements.length; ++i){
-      elements[i].style.display = 'block';
-    }
+    
+    document.getElementsByClassName('lead__title_en')[0].style.display = 'none';
+    document.getElementsByClassName('lead__title_ru')[0].style.display = 'block';
+    document.getElementsByClassName('lead__subtitle_en')[0].style.display = 'none';
+    document.getElementsByClassName('lead__subtitle_ru')[0].style.display = 'block';
+    document.getElementsByClassName('lead__caption_en')[0].style.display = 'none';
+    document.getElementsByClassName('lead__caption_ru')[0].style.display = 'block';
+
+    document.getElementsByClassName('intro__title_en')[0].style.display = 'none';
+    document.getElementsByClassName('intro__title_ru')[0].style.display = 'block';
+    document.getElementsByClassName('intro__text_en')[0].style.display = 'none';
+    document.getElementsByClassName('intro__text_ru')[0].style.display = 'block';
+    document.getElementsByClassName('intro__list_en')[0].style.display = 'none';
+    document.getElementsByClassName('intro__list_ru')[0].style.display = 'block';
+
+    document.getElementsByClassName('place__paragraph_en')[0].style.display = 'none';
+    document.getElementsByClassName('place__paragraph_ru')[0].style.display = 'block';
 }
+
